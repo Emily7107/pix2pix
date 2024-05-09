257cab6cc8f8f39a4bfcade946560cd043a0e135
Emily7107 <emily891024@gmail.com>
2024-05-09 23:59:36 +0200

    09/05/2024

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ args = parser.parse_args()
 
 device = ('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-transforms = T.Compose([T.CenterCrop((128,128)),
+transforms = T.Compose([T.CenterCrop((256,256)),
                         T.ToTensor(),
                         T.Normalize(mean=[0.5, 0.5, 0.5],
                                      std=[0.5, 0.5, 0.5]),])
